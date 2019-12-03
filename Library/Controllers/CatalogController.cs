4b7db3f3f0fc331c522fbabdda2c528c892b6639
V-Adhish Kapoor <Adhish.Kapoor@blackbaud.me>
2019-12-03 16:36:22 +0530

    latest commit

--- Library/Controllers/CatalogController.cs
+++ Library/Controllers/CatalogController.cs
@@ -1,9 +1,7 @@
-﻿using LibraryData;
+﻿using Library.Models.Catalog;
+using LibraryData;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Library.Controllers
 {
@@ -18,6 +16,23 @@ namespace Library.Controllers
         {
             var assetModels = _assets.GetAll();
 
+            var listingResult = assetModels
+                .Select(result => new AssetIndexListingModel
+                {
+                    Id = result.Id,
+                    ImageUrl = result.ImageUrl,
+                    AuthorOrDirector = _assets.GetAuthorOrDirector(result.Id),
+                    DeweyCallNumber = _assets.GetDeweyIndex(result.Id),
+                    Title = result.Title,
+                    Type = _assets.GetType(result.Id)
+
+                });
+            var model = new AssetIndexModel()
+            {
+                Assets = listingResult
+            };
+
+            return View(model);
         }
     }
 }
